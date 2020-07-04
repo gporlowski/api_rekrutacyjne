2c7369d22426df7d2f4cff700bc344c94c880f04
Grzegorz Orlowski <g.p.orlowski@gmail.com>
2020-07-04 12:00:24 +0200

    loadAndSetCurrency

--- src/main/java/com/sda/javagdy4/apip_rekrutacyjne/Main.java
+++ src/main/java/com/sda/javagdy4/apip_rekrutacyjne/Main.java
@@ -1,7 +1,23 @@
 package com.sda.javagdy4.apip_rekrutacyjne;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        NBPApiParameters parameters = new NBPApiParameters();
 
+        loadAndSetCurrency(scanner, parameters);
+    }
+
+    private static void loadAndSetCurrency(Scanner scanner, NBPApiParameters parameters) {
+        do {
+            System.out.print("Please enter currency [dollar, euro, ruble]: ");
+            Optional<NBPCurrency> optionalCurrency = NBPCurrency.parse(scanner.nextLine());
+            if(optionalCurrency.isPresent()) {
+                parameters.setCurrency(optionalCurrency.get());
+            }
+        } while (parameters.getCurrency() == null); // wykonuj pętle, dopóki currency == null
     }
 }
---
+++ src/main/java/com/sda/javagdy4/apip_rekrutacyjne/NBPCurrency.java
@@ -0,0 +1,30 @@
+package com.sda.javagdy4.apip_rekrutacyjne;
+
+import java.util.Optional;
+
+public enum NBPCurrency {
+    UNITED_STATES_DOLLAR("USD"),
+    RUSSIAN_RUBLE("RUB"),
+    EURO("EUR");
+
+    private String shortName;
+
+    NBPCurrency(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public static Optional<NBPCurrency> parse(String input) {
+        if(input.toUpperCase().contains("DOLLAR")) {
+            return Optional.of(NBPCurrency.UNITED_STATES_DOLLAR);
+        } else if (input.toUpperCase().contains("RUBLE")) {
+            return Optional.of(NBPCurrency.RUSSIAN_RUBLE);
+        } else if (input.toUpperCase().contains("EURO")) {
+            return Optional.of(NBPCurrency.EURO);
+        }
+        return Optional.empty();
+    }
+}
